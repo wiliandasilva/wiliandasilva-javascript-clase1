7dc89e506b178d4935c08a6268e37408f805fc2a
wilian gonzalez da silva <wilian.da.silva97@gmail.com>
2021-10-13 20:45:50 -0300

    clase 1

---
+++ main.js
@@ -0,0 +1,19 @@
+const Año = 2021;
+
+let nombre = prompt("Ingrese su Nombre");
+ 
+let apellido = prompt("Ingrese su Apellido");
+
+let fechaNacimiento = prompt("Ingrese su fecha de Nacimiento");
+
+parseInt (Año);
+
+parseInt (fechaNacimiento);
+
+let edad = Año - fechaNacimiento;
+
+alert ("Hola "+ nombre + " " + apellido + "\nEdad: " + edad + "\nBienvenido al curos de JavaScript!" )
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad + " Años");
